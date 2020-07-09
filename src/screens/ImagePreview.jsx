5f93ae01b1181aea43b914b0c223c4919ad74255
Jassu <jaswanthsaisattenapalli@gmail.com>
2020-07-09 16:18:59 +0530

    new yarn.lock

--- src/screens/ImagePreview.jsx
+++ src/screens/ImagePreview.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Image, View } from 'react-native';
+import { toByteArray } from 'base64-js';
+
+// function convertDataURIToBinary(base64) {
+//   var raw = atob(base64);
+//   var rawLength = raw.length;
+//   var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+//   for (i = 0; i < rawLength; i++) {
+//     array[i] = raw.charCodeAt(i);
+//   }
+//   return array;
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -21,6 +33,23 @@ const styles = StyleSheet.create({
 });
 
 const ImagePreview = ({ route }) => {
+  useEffect(() => {
+    // console.log(route.params.base64);
+    const rawData = toByteArray(route.params.photo.base64);
+    const arr = [];
+    let temp = [];
+    for (let i = 0; i < rawData.length; i += 3) {
+      temp = [];
+      temp.push(rawData[i]);
+      temp.push(rawData[i + 1]);
+      temp.push(rawData[i + 2]);
+      arr.push(temp);
+    }
+    // console.log(rawData.length);
+    // console.log(route.params.photo.width);
+    // console.log(route.params.photo.height);
+  }, [route.params.photo.base64]);
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={{ uri: route.params.photo.uri }} />
